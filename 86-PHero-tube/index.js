ab3f43c40ad0d275d76acb1f214ad7dfed21c108
xunaiet <xunaiet28@gmail.com>
2023-09-01 22:33:28 +0600

    last commit

--- 86-PHero-tube/index.js
+++ 86-PHero-tube/index.js
@@ -20,34 +20,41 @@ const containerCard = async (id) => {
     const datainfo = data.data;
     const lastbtn = document.getElementById('last')
     const cardall = document.getElementById('card-container')
-    cardall.textContent ='';
-    if(datainfo.length == 0){
+    cardall.textContent = '';
+    if (datainfo.length == 0) {
 
         lastbtn.classList.remove("hidden")
-    }else{
+    } else {
         lastbtn.classList.add('hidden')
     }
-    datainfo.forEach((carddata) => { 
-    
+    datainfo.forEach((carddata) => {
+
         // console.log(cardall)
 
         const imageShort = carddata?.authors[0];
         const image = imageShort?.profile_picture;
         const image_name = imageShort?.profile_name
         console.log(image_name)
-
+        const allTimer = carddata?.others?.posted_date
+        let hours = Math.floor(allTimer / 60);
+        let minites = hours / 60;
+        let minite = parseInt(minites);
+        let hoursFloat = parseFloat(minite)
+        let miniteresult = allTimer % 60;
         const createcard = document.createElement('div');
         createcard.innerHTML = `
-       <div class="card card-compact w-72 bg-base-100">
+       <div class="card card-compact w-72 bg-base-100 relative">
+       
     <figure><img class="h-[200px] rounded-md" src="${carddata.thumbnail}" alt="Shoes" /></figure>
     <div class="items-center mt-6 gap-4 flex justify-start">
-     
+      <P class=" absolute -mt-32 ml-44 bg-[#171717] text-[#FFF]" id="convert"> ${hoursFloat ? hoursFloat : ''}<span>${hoursFloat ? 'hrs' : ''}</span>${miniteresult ? miniteresult : ''}
+      <span>${miniteresult ? 'min ago' : ''}</span></P>
         <img class="w-10 h-10 rounded-3xl" src="${image}">
         <p class="font-bold"> ${carddata.title}</p>
         </div>        
         <div class="flex justify-start ml-14 gap-4">
-       <span> ${carddata.authors[0].profile_name}</span>${carddata.authors[0].verified?
-         '<img src="img.svg" alt="" id="logo">':''}
+       <span> ${carddata.authors[0].profile_name}</span>${carddata.authors[0].verified ?
+                '<img src="img.svg" alt="" id="logo">' : ''}
       </div>
             <p class="text-gray ml-14"> ${carddata.others?.views} views</p>
        </div>`
@@ -56,9 +63,9 @@ const containerCard = async (id) => {
     })
 }
 
-document.getElementById('btnquestion').addEventListener('click', function(){
+document.getElementById('btnquestion').addEventListener('click', function () {
 
-window.location.href='question.html'
+    window.location.href = 'question.html'
 
 })
 
